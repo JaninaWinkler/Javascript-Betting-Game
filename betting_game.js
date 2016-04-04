4e9d689436cbad27187e96bd753f5eced9986e94
JaninaWinkler <nina.winkler@hotmail.com>
2016-04-04 20:46:22 +0000

    add JS to HTML script

--- betting_game.js
+++ betting_game.js
@@ -16,32 +16,57 @@
 // - prompt
 // - alert
 
-function bettingGame() {
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  var bankroll = 100;
-  playing = true;
-  
-  function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+function promptForValue() {
+  return prompt("Please enter a value between 5 and 10 that you want to bet: ");
+}
+
+function promptForNumber() {
+  return prompt("Please enter a number between 1 and 10 that you want to bet on: ");
+} 
+
+function playGame(bankroll) {
   var randomNumber = getRandomNumber(0, 10);
-  var value = prompt("Please enter a value between 5 and 10 that you want to bet: ");
-  var userNumber = prompt("Please enter a number between 1 and 10 that you want to bet on: ");
-  
+  var value = promptForValue();
+  while (value < 5 || value > 10) {
+    value = promptForValue();
+  }
+  var userNumber = promptForNumber();
+  while (userNumber < 1 || userNumber > 10) {
+    userNumber = promptForNumber();
+  }
   if (userNumber == randomNumber) {
-    var bankroll = bankroll + value;
+    bankroll = bankroll + value;
     alert("Good job! You've gained $" + value + " and your bankroll is now $" + bankroll);
   }
   else if (userNumber == (randomNumber + 1) || userNumber == (randomNumber -1)) {
     alert("Almost! Because you were SO close, your bankroll stays at $" + bankroll);
   }
   else if (userNumber != randomNumber) {
-    var bankroll = bankroll - value;
+    bankroll = bankroll - value;
     alert("Nope! You've lost $" + value + " and your bankroll is now $" + bankroll);
+    bankroll = bankroll 
   }
+  return bankroll;
+}
+
+function bettingGame() {
+
+  var bankroll = 100;
+  playing = true;
+
+  while (playing) {  
     if (bankroll <= 0) {
       playing = false;
+      alert("Game Over. Sorry!");
+      break;
+    } else {
+      bankroll = playGame(bankroll);
     }
+  }
 }
 
 bettingGame();
